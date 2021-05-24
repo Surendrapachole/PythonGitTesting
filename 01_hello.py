31eae650544082d29039312a7f6e7de17c8fbed0
Surendrapachole <surendra.nitk@yahoo.com>
2021-05-24 18:35:26 +0530

    new version for jenkins jobs

--- 01_hello.py
+++ 01_hello.py
@@ -2,4 +2,4 @@
 This program is to test Git - Jenkins - simple pipeline*********
 '''
 print("Hello",end =" ")# use end=" "  to print the output in same line
-print("This is beein getting pulled from GIt and running in Jenkins")
+print("This is beeing getting pulled from GIt and running in Jenkins")
