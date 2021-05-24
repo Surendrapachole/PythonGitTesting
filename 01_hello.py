ea1b2b60de389d008b6cddc4336a93f9e049fd7c
Surendrapachole <surendra.nitk@yahoo.com>
2021-05-24 07:17:01 +0530

    This file is to test Git Jenkins test, first version

---
+++ 01_hello.py
@@ -0,0 +1,5 @@
+'''
+This program is to test Git - Jenkins - simple pipeline*********
+'''
+print("Hello world",end =" ")# use end=" "  to print the output in same line
+print(", Hello world again in same line")
